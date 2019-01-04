49d81e0f108bec6a3fe01d79b185b18cfbdbf629
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-04 02:06:48 -0500

    Removed template comments from bloc_flux.dart. Added the rxdart and meta dependencies to pubspec.yaml

--- example/bloc_flux_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bloc_flux/bloc_flux.dart';
-
-main() {
-  var awesome = new Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/bloc_flux.dart
+++ lib/bloc_flux.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// TODO: add dart docs.
 library bloc_flux;
-
-export 'src/bloc_flux_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
