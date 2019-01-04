8711fea0bd08daf0852d298b4f19bea8b13b5898
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-04 03:18:18 -0500

    Added basic Actions ErrorAction and InitialAction and the ParentAction mixin to the action.dart file.

--- lib/src/action.dart
+++ lib/src/action.dart
@@ -8,3 +8,23 @@ abstract class Action<T> {
 
   Action({this.data});
 }
+
+//TODO: check if using a empty mixin as a kind of identifier is bad practice or if there is a better way to do this.
+///This mixin indicates that an Action should be handled in the
+///handleParentAction callback method of a StoreBloc rather than being passed
+///down to the actionStream and handleAction callback method of a subclass.
+mixin ParentAction {}
+
+///Extend this for Actions that can have errors.
+@immutable
+abstract class ErrorAction<T, E> extends Action<T> {
+  ///An object representing the error that occurred.
+  final E error;
+
+  ErrorAction({T data, this.error}): super(data: data);
+}
+
+@immutable
+class InitialAction<T> extends Action<T> with ParentAction {
+
+}
