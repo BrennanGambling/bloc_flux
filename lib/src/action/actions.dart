a9cacff796f24acb3c7265871a2042a0517d033e
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-20 00:12:19 -0500

    Added documentation to the actions.dart file.

--- lib/src/action/actions.dart
+++ lib/src/action/actions.dart
@@ -1,7 +1,5 @@
 import 'package:meta/meta.dart';
 
-//TODO: write proper documentation.
-
 ///Indicates that something that could potentially affect the application state.
 ///
 ///All [Action]s must either extend or implement this class.
@@ -16,31 +14,50 @@ abstract class Action<T> {
   Action({this.data});
 }
 
-//action decorator adds an E error field.
-//if the [action] has fields other than T data these can
-//be accessed from the action itself.
+//The Decorator pattern is used so any Action can be decorated as an ErrorAction
+//without having to extend or implement this class.
+
+///A Decorator for the [Action] class which indicates the request [Action]
+///resulted in an error represented by [E] [error].
+///
+///If the [ErrorAction] does not require a payload [error] can be left null.
 @immutable
 abstract class ErrorAction<T, E> implements Action<T> {
+  ///The [Action<T>] to wrap.
   final Action<T> action;
+
+  ///The error payload. May be null.
   final E error;
 
-  //override the data field as this Action's data field will be null.
-  //(it was not set with the super constructor.)
+  ///Creates an [ErrorAction] wrapping [Action] [action] with payload [error].
   ErrorAction(this.action, this.error);
 
+  //data is overriden as the super constrctor is called implicitly with no
+  //parameters meaning the super classes data field will always be null.
+
+  ///The [data] payload from the wrapped [Action].
   @override
   T get data => action.data;
 }
 
-//marker interface for actions dispatched from other blocs
+//TODO: add square brackets around bloc or whatever the final name for the
+//most basic bloc class is.
+
+///Marker interface indicating an [Action] was dispatched from another bloc.
+///
+///DO NOT extend this class because the super classes data field will be left
+///null as the super constructor is not called.
+///
+///In the future this will be used to distinguish user or externally
+///dispatched [Action]s for logging and state serialization purposes.
 @immutable
 abstract class InternalAction<T> extends Action<T> {}
 
-//an action that carries a value from another bloc.
-//this value must not be null.
+//TODO: add square brackets around bloc or whatever the final name for the
+//most basic bloc class is.
+
+///An [Action] indicating a new value from a bloc.
 @immutable
 abstract class ValueAction<T> extends Action<T> implements InternalAction<T> {
-  ValueAction(T data)
-      : assert(data != null, "T data must NOT be null."),
-        super(data: data);
+  ValueAction(T data) : super(data: data);
 }
