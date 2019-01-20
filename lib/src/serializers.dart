c6963cfefa0142f74e1666d978f589dd7e12054f
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-20 07:53:41 -0500

    Fixed a bug in the isSerializable method in serilizers.dart that would casue a StateError to throw no matter the type given.

--- lib/src/serializers.dart
+++ lib/src/serializers.dart
@@ -1,6 +1,9 @@
 library serializers;
 
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/standard_json_plugin.dart';
 import 'package:built_value/serializer.dart';
+import 'state/bloc_state.dart';
 import 'state/field_state.dart';
 
 part 'serializers.g.dart';
@@ -8,7 +11,8 @@ part 'serializers.g.dart';
 //TODO: add proper documentation.
 
 bool isSerializable(Type type, {bool shouldThrow: true}) {
-  final bool serializable = serializers.serializerForType(type) != null;
+  print(type);
+  final bool serializable = serializers.serializerForType(type) != null || type == Object;
   if (!serializable && shouldThrow) {
     //TODO: add link to more information about this error.
     //basically that only the primitives, Built and BuiltCollections are serializable.
@@ -18,6 +22,8 @@ bool isSerializable(Type type, {bool shouldThrow: true}) {
 }
 
 @SerializersFor(const [
+  BlocState,
   FieldState
 ])
 final Serializers serializers = _$serializers;
+final Serializers standardSerializers = (serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
