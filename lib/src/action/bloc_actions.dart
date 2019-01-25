077af3b915f73c3e5a41905aa7a660d5c4bde00f
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-25 15:48:04 -0500

    added comments to the bloc_actions.dart file

--- lib/src/action/bloc_actions.dart
+++ lib/src/action/bloc_actions.dart
@@ -5,8 +5,14 @@ import 'actions.dart';
 
 //TODO: implement these classes.
 
+//TODO: this class should have the data type as a field query so if specified
+//only specific states will be returned. and then a key property for the key of the bloc
 //class RequestBlocStateAction implements InternalAction {}
 
+//set the bloc state
+//T = BlocState
 //class BlocStateAction implements InternalAction {}
 
+//broadcast the last BlocState
+//T = BlocState
 //class BlocStateValueAction extends ValueAction {}
