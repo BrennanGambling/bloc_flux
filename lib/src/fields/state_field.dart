92fd81c0abdd41c6ef48d3157f175f7b26021781
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-27 20:09:08 -0500

    Added todo notes for new field classes.

---
+++ lib/src/fields/state_field.dart
@@ -0,0 +1,6 @@
+//TODO: state field derived constructor
+//if a state field is derived it converts the same data stored in another field.
+//basically its input field will be the out put of another field
+
+//the value im a derived state field will not be backed up or restored as it 
+//will be update when its parent field is updated.
---
+++ lib/src/fields/value_field.dart
@@ -0,0 +1,3 @@
+//TODO: value field derived constructor
+//if a value field is derived it converts the same data stored in another field.
+//basically its input field will be the out put of another field
