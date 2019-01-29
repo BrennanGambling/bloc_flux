e877fbfe937687bd4fce3f843567f964eb83bac3
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-29 15:58:59 -0500

    Added FieldQueryAction.

--- lib/src/action/field_actions.dart
+++ lib/src/action/field_actions.dart
@@ -1,6 +1,7 @@
 import 'package:meta/meta.dart';
 import 'actions.dart';
 import '../field_id.dart';
+import '../field_query.dart';
 
 ///An [Action] indicating an update to a [Field] that a bloc is subscribed to.
 @immutable
@@ -8,4 +9,15 @@ class FieldValueAction<T> extends ValueAction<T> {
   final FieldID fieldID;
 
   FieldValueAction(T data, this.fieldID) : super(data);
+}
+
+///An [Action] indicating a request for field subscriptions.
+@immutable
+class FieldQueryAction extends Action<FieldQuery> implements InternalAction<FieldQuery> {
+  ///Helper method for getting the [FieldQuery]s blocKey.
+  String get blocKey => data.blocKey;
+  ///Helper method for getting the FieldQuery.
+  FieldQuery get fieldQuery => data;
+
+  FieldQueryAction(FieldQuery fieldQuery) : super(data: fieldQuery);
 }
