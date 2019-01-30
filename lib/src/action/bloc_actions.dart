7633dd8d7a19afeca587de25253e20092c28f2fa
Brennan Gambling <brennan.gambling@gmail.com>
2019-01-30 16:35:05 -0500

    Added Actions: BlocStateAction, BlocStateValueAction and StateQueryAction.

--- lib/src/action/bloc_actions.dart
+++ lib/src/action/bloc_actions.dart
@@ -1,18 +1,70 @@
 import 'package:meta/meta.dart';
-import 'package:built_value/built_value.dart';
 
+import '../state/bloc_state.dart';
+import '../state_query.dart';
 import 'actions.dart';
 
-//TODO: implement these classes.
+///Dispatch an instance of this [Action] to set the state of a [StateBloc].
+///
+///DO NOT use this [Action] to communicate a state change. This [Action] SETS
+///the state of a [StateBloc]. Use [BlocStateValueAction] to communicate a state
+///change.
+///
+///[blocState] must NOT be null.
+@immutable
+class BlocStateAction extends Action<BlocState>
+    implements QueryAction<BlocState> {
+  ///[blocState] must NOT be null.
+  BlocStateAction(BlocState blocState) : super(data: blocState) {
+    if (blocState == null) {
+      throw ArgumentError.notNull("blocState must not be null.");
+    }
+  }
 
-//TODO: this class should have the data type as a field query so if specified
-//only specific states will be returned. and then a key property for the key of the bloc
-//class RequestBlocStateAction implements InternalAction {}
+  ///Helper method for getting the blocKey.
+  String get blocKey => data.key;
 
-//set the bloc state
-//T = BlocState
-//class BlocStateAction implements InternalAction {}
+  ///Helper method for getting the blocState.
+  BlocState get blocState => data;
+}
 
-//broadcast the last BlocState
-//T = BlocState
-//class BlocStateValueAction extends ValueAction {}
+///Dispatch an instance of this [Action] to communicate a state change in a
+///[StateBloc].
+///
+///DO NOT use this [Action] to set the state of a [StateBloc]. This [Action]
+///COMMUNICATES a state change. Use [BlocStateAction] to set the state of a
+///[StateBloc].
+///
+///[blocState] must NOT be null.
+@immutable
+class BlocStateValueAction extends ValueAction<BlocState>
+    implements QueryAction<BlocState> {
+  ///blocState must NOT be null.
+  BlocStateValueAction(BlocState blocState) : super(blocState);
+
+  ///Helper method for getting the blocKey.
+  String get blocKey => data.key;
+
+  ///Helper method for getting the blocState.
+  BlocState get blocState => data;
+}
+
+///Dispatch an instance of this [Action] to register a [StateQuery].
+///
+///[stateQuery] must NOT be null.
+@immutable
+class StateQueryAction extends Action<StateQuery>
+    implements QueryAction<StateQuery> {
+  ///[stateQuery] must NOT be null.
+  StateQueryAction(StateQuery stateQuery) : super(data: stateQuery) {
+    if (stateQuery == null) {
+      throw ArgumentError.notNull("stateQuery must not be null.");
+    }
+  }
+
+  ///Helper method for getting the blocKey.
+  String get blocKey => data.blocKey;
+
+  ///Helper method for getting the stateQuery.
+  StateQuery get stateQuery => data;
+}
