6c1fbdab5fe5a3ce859e5e0e6c6e7457b5e222a9
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-03 03:45:59 -0500

    Added documentation and TODO comments to StateBlocImpl. Removed call in constructor to init().

--- lib/src/blocs/impl/state_bloc_impl.dart
+++ lib/src/blocs/impl/state_bloc_impl.dart
@@ -14,13 +14,20 @@ import '../state_bloc.dart';
 import 'value_bloc_impl.dart';
 
 abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
+  ///The map of all [StateField] [FieldID]s to registered [StateField]s.
   @protected
   final Map<FieldID, StateField> stateFieldMap;
 
+  ///@nodoc
+  ///Internal variable for [activeStateQueries] getter and setter.
   int _activeStateQueries;
 
+  ///@nodoc
+  ///Internal variable for [forceDispatch] getter and setter.
   bool _forceDispatch;
 
+  ///@nodoc
+  ///Internal variable for method [permanentForceDispatch()] and getter [isPermanentForceDispatch].
   bool _permanentForceDispatch;
 
   ///All incoming [StateQuery]s with this [StateBloc]'s key.
@@ -31,12 +38,38 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
   @protected
   final Observable<StateBlocState> blocStateObservable;
 
+  ///@nodoc
+  ///The [StreamSubscription] for the [stateQuery()] onData listener of the
+  ///[stateQueryObservable].
+  ///
+  ///This [StreamSubscription] is cancelled in the [dispose()] method.
   StreamSubscription _stateQuerySubscription;
 
+  ///@nodoc
+  ///The [StreamSubscription] for the [setState()] onData listener of the
+  ///[blocStateObservable].
+  ///
+  ///This [StreamSubscription] is cancelled in the [dispose()] method.
   StreamSubscription _blocStateSubscription;
 
+  ///@nodoc
+  ///Internal variable for the [dispatchState] getter and setter.
   bool _dispatchState;
 
+  /*TODO: initialState should only be checked to see if it is valid AFTER
+  the child constructor is finished. Otherwise the initialState will
+  always be invalid as the fields have not yet been added to the map.
+  The initial state should probably be checked and sent to setState when the
+  bloc is registered with the Dispatcher via a special action.*/
+
+  ///Creates an instance of this class with the unique identifier [key] and
+  ///the input [actionObservable] from the [Dispather].
+  ///
+  ///An initialState can be set by specifiy [initialState]. The initial state
+  ///must have the a [StateBlocState.blocKey] equal to [key]. All [FieldState]s
+  ///in the initial state must also have [StateField]s in the [stateFieldMap]
+  ///with equal [FieldID]s. The initial state of the registered [StateField]s
+  ///will not be set until this is registered with a [Dispatcher].
   StateBlocImpl(String key, Observable<Action> actionObservable,
       {StateBlocState initialState, bool forceDispatch: false})
       : stateFieldMap = Map(),
@@ -56,7 +89,6 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
         throw InvalidStateBlocStateError(this, initialState);
       }
     }
-    init();
   }
 
   ///The number of [StateQueries] currently registered for this [StateBloc].
@@ -98,7 +130,10 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
   ///has been called one or more times calls to this method will not effect the
   ///value returned by the [forceDispatch] getter. This can be checked using
   ///the [isForceDispatchPermanent()] method.
-  set forceDispatch(bool forceDispatch) => _forceDispatch;
+  set forceDispatch(bool forceDispatch) { 
+    _forceDispatch;
+    _updateDispatchState();
+  }
 
   ///Whether or not forceDispatch is permanently set to true.
   ///
@@ -117,6 +152,7 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
     //TODO: enable and disable state dispatching.
   }
 
+  ///Call this method to perform clean up when this is no longer needed.
   @override
   @mustCallSuper
   void dispose() {
@@ -125,6 +161,8 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
     _blocStateSubscription?.cancel();
   }
 
+  /*TODO: add this method to bloc class. The method should be called when
+  the first initial action from the dispatcher is received.*/
   ///This method is called as the last statement in the constructor.
   ///
   ///Override to perform any initialization work.
@@ -199,12 +237,26 @@ abstract class StateBlocImpl extends ValueBlocImpl implements StateBloc {
   @mustCallSuper
   void stateUpdated(StateBlocState stateBlocState) {}
 
+  ///@nodoc
+  ///Sets [_stateQuerySubscription] to the [StreamSubscription] returned when
+  ///[stateQuery()] is added as an onData listener for the [stateQueryObservable].
   void _createQuerySubscription() =>
       _stateQuerySubscription = stateQueryObservable.listen(stateQuery);
 
+  ///@nodoc
+  ///Sets [_blocStateSubscription] to the [StreamSubscription] returned when
+  ///[setState()] is added as an onData listener for the [blocStateObservable].
   void _createStateSubscription() =>
       _blocStateSubscription = blocStateObservable.listen(setState);
 
+  ///@nodoc
+  ///Sets the _dispatchState if it has changed as a result of a method call and
+  ///calls [dispatchStateChanged()].
+  ///
+  ///Called by:
+  ///1. [activeStateQueries]
+  ///2. [forceDispatch]
+  ///3. [permanentForceDispatch]
   void _updateDispatchState() {
     final bool newDispatchState = _activeStateQueries > 0 || _forceDispatch;
     if (_dispatchState != newDispatchState) {
