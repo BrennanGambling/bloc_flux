21db62e52ddea2217ece693abe67d5314c4f5cdc
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-03 19:56:55 -0500

    Added all of the classes in package.

--- lib/bloc_flux.dart
+++ lib/bloc_flux.dart
@@ -5,9 +5,22 @@ library bloc_flux;
 
 export 'src/action/actions.dart';
 export 'src/action/bloc_actions.dart';
+export 'src/action/field_actions.dart';
+export 'src/blocs/bloc.dart';
+export 'src/blocs/impl/bloc_impl.dart';
+export 'src/blocs/impl/state_bloc_impl.dart';
+export 'src/blocs/impl/value_bloc_impl.dart';
+export 'src/blocs/state_bloc.dart';
+export 'src/blocs/value_bloc.dart';
 export 'src/field_id.dart';
+export 'src/field_query.dart';
+export 'src/fields/field.dart';
+export 'src/fields/impl/field_impl.dart';
+export 'src/fields/impl/state_field_impl.dart';
+export 'src/fields/state_field.dart';
 export 'src/serializers.dart';
 export 'src/state/bloc_state.dart';
 export 'src/state/field_state.dart';
+export 'src/state_query.dart';
 
 // TODO: Export any libraries intended for clients of this package.
