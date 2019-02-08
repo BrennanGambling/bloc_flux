078043d57245ea167567dcec5f159bc70438300f
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-08 07:35:57 -0500

    Fixed a few bugs in Field and FIeldImpl. Added fieldID getter to FieldView. Updated the doc comments.

--- lib/src/field/impl/field_impl.dart
+++ lib/src/field/impl/field_impl.dart
@@ -8,79 +8,94 @@ import '../../bloc/impl/bloc_impl.dart';
 import '../../field_id.dart';
 import '../field.dart';
 
+///The implementation for [Field].
 class FieldImpl<T> implements Field<T> {
-  @override
-  final String key;
-
+  ///{@macro field_fieldID_getter}
   @override
   final FieldID fieldID;
 
+  ///{@macro field_derived_getter}
+  ///
+  ///{@macro only_call_from_bloc}
   @override
   final bool derived;
 
+  ///{@macro field_observable_getter}
   @override
   final ValueObservable<T> observable;
 
+  ///The [BehvaiorSubject] managing [observable].
   @protected
   final BehaviorSubject<T> subject;
 
+  ///@nodoc
+  ///The [StreamSubscription] managing the [subject.add] onData listener
+  ///added to the [inputObservable].
   @protected
-  StreamSubscription<T> inputSubscription;
+  StreamSubscription<T> _inputSubscription;
 
-  Observable<T> _inputObservable;
+  ///The [Observable] carrying input values.
+  final Observable<T> inputObservable;
 
-  //Stream subscription managing the inputObserbable
-  //this needs to be canceled when the inputObservable is changed.
+  ///@nodoc
+  ///Internal [FieldView] created with this [FieldImpl] for use by the [fieldView]
+  ///getter.
+  ///
+  ///This is set in the constructor.
+  FieldView<T> _fieldView;
+
+  ///{@macro field_constructor}
   FieldImpl(String key, String blocKey, Observable<T> inputObservable,
       bool derived, BlocImpl bloc)
-      : this._(key, FieldID(blocKey, key), inputObservable, BehaviorSubject(),
+      : this._(FieldID(blocKey, key), inputObservable, BehaviorSubject(),
             derived, bloc);
 
-  FieldImpl._(this.key, this.fieldID, this._inputObservable, this.subject,
-      this.derived, BlocImpl bloc)
+  FieldImpl._(this.fieldID, this.inputObservable, this.subject, this.derived,
+      BlocImpl bloc)
       : observable = subject.stream {
-    inputObservableChanged();
+    _createInputSubscription();
     if (bloc != null) {
       bloc.addField(this);
     }
+    _fieldView = FieldView(this);
   }
 
-  Observable<T> get inputObservable => _inputObservable;
+  ///{@macro fieldView_getter}
+  ///
+  ///{@macro call_only_from_bloc}
+  @override
+  FieldView<T> get fieldView => _fieldView;
 
-  //local input observable
-  set inputObservable(Observable<T> observable) {
-    //when the observable is changed the old subscription needs to be cancelled
-    //and a new one created.
-    _inputObservable = observable;
-    inputObservableChanged();
-  }
-
+  ///{@macro field_lastValue_getter}
   @override
   T get lastValue => subject.value;
+
+  ///{@macro field_add}
+  ///
+  ///{@macro field_add}
+  @override
   void add(T data) => subject.add(data);
 
+  ///{@macro field_dispose}
+  ///
+  ///{@macro only_call_from_bloc}
   @override
   @mustCallSuper
   void dispose() {
-    inputSubscription?.cancel();
+    _inputSubscription?.cancel();
     subject.close();
   }
 
-  //override to preform other work when the input obersvable changes.
+  ///{@macro get_value_action}
+  ///
+  ///{@macro only_call_from_bloc}
   @override
   FieldValueAction<T> getTypedValueAction(T data) =>
       FieldValueAction<T>(data, this.fieldID);
 
-  @mustCallSuper
-  @protected
-  void inputObservableChanged() {
-    //when the input observable changes the current subscription for the old
-    //one needs to be canceled (if there was one) and the new one needs to be listened to.
-    inputSubscription?.cancel();
-    inputSubscription = _inputObservable.listen(subject.add);
-  }
-
   ///{@macro is_valid_type}
+  ///
+  ///{@macro only_call_from_bloc}
   @override
   bool isValidType(dynamic data) {
     try {
@@ -89,5 +104,11 @@ class FieldImpl<T> implements Field<T> {
       return false;
     }
     return true;
+  }
+
+  ///@nodoc
+  ///Creates the [_inputSubscription].
+  void _createInputSubscription() {
+    _inputSubscription = inputObservable.listen(subject.add);
   }
 }
