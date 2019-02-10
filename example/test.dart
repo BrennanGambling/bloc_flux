251ae606168908298f2c00a3daac31b775cf017c
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-09 19:14:46 -0500

    added testing example (this will be removed later).

---
+++ example/test.dart
@@ -0,0 +1,29 @@
+import 'package:bloc_flux/bloc_flux.dart';
+import 'dart:convert';
+
+void main() {
+  //keys for BlocState.
+  const String blocKey = "blocKey";
+  //keys for FieldState.
+  const String fieldKey1 = "fieldKey1";
+  final FieldID fieldID1 = FieldID(blocKey, fieldKey1);
+  const String fieldKey2 = "fieldKey2";
+  final FieldID fieldID2 = FieldID(blocKey, fieldKey2);
+
+  //create 2 FieldStates
+  FieldState<String> field1 = FieldState(fieldID1, "someData");
+  FieldState<int> field2 = FieldState(fieldID2, 4);
+
+  //Create a Map of keys to FieldStates.
+  final Map<FieldID, FieldState> map = Map();
+  map[field1.fieldID] = field1;
+  map[field2.fieldID] = field2;
+
+  //Create a BlocState using the basic constructor.
+  final StateBlocState blocState = StateBlocState.fromMap(blocKey, map);
+
+  final String serialized = jsonEncode(blocFluxSerializers.serializeWith(StateBlocState.serializer, blocState));
+  final StateBlocState deserialized = blocFluxSerializers.deserializeWith(StateBlocState.serializer, jsonDecode(serialized));
+
+  assert(blocState == deserialized);
+}
