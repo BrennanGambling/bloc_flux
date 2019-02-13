781980186d751bb5548a6ee2dd9ef9e04f5c0be9
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-13 17:27:13 -0500

    Added composite_serializers.dart to the linrary in top level serializers.dart.

--- lib/serializers.dart
+++ lib/serializers.dart
@@ -1,6 +1,7 @@
 ///All classes for serialization of serializable types.
 library serializers;
 
+export 'src/serializers/composite_serializers.dart';
 export 'src/serializers/serializers.dart';
 
 //TODO: add macro from serializers in src with list of serializable types.
