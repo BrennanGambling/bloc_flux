b625677723eedc8f52e9505e62fa297bb4b66ca1
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-24 03:04:56 -0500

    Added more helper methods to FieldQueryAction.

--- lib/src/action/field_actions.dart
+++ lib/src/action/field_actions.dart
@@ -1,4 +1,6 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:meta/meta.dart';
+import 'package:quiver/core.dart';
 
 import '../field/field_id.dart';
 import '../query/field_query.dart';
@@ -17,11 +19,29 @@ class FieldQueryAction extends Action<FieldQuery>
     }
   }
 
+  ///Helper method for getting the [FieldQuery.all] from [fieldQuery].
+  bool get all => data.all;
+
   ///Helper method for getting the [FieldQuery.blocKey] from [fieldQuery].
   String get blocKey => data.blocKey;
 
+  ///Helper method for getting the [FieldQuery.cancel] from [fieldQuery].
+  bool get cancel => data.cancel;
+
+  ///Helper method for getting the [FieldQuery.fieldIDs] from [fieldQuery].
+  BuiltList<FieldID> get fieldIDs => data.fieldIDs;
+
+  ///Helper method for getting the [FieldQuery.fieldKeys] from [fieldQuery].
+  BuiltList<String> get fieldKeys => data.fieldKeys;
+
   ///Helper method for getting the [fieldQuery].
   FieldQuery get fieldQuery => data;
+
+  ///Helper method for getting the [FieldQuery.single] from [fieldQuery].
+  bool get single => data.single;
+
+  ///Helper method for getting the [FieldQuery.subscription] from [fieldQuery].
+  bool get subscription => data.subscription;
 }
 
 ///A [ValueAction] indicating an update to a [Field] that a [FieldQuery]
@@ -41,4 +61,13 @@ class FieldValueAction<T> extends ValueAction<T> implements QueryAction<T> {
       throw ArgumentError("fieldID must NOT be null.");
     }
   }
+
+  @override
+  int get hashCode => hash2(this.data, this.fieldID);
+
+  @override
+  bool operator ==(dynamic other) =>
+      (other is FieldValueAction<T>) &&
+      (this.data == other.data) &&
+      (this.fieldID == other.fieldID);
 }
