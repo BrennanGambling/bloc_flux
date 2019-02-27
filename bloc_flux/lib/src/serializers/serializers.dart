7c3ddc5c653429c898bcf71abb3709e4f643e5e1
Brennan Gambling <brennan.gambling@gmail.com>
2019-02-26 20:53:34 -0500

    Removed unused import and deleted broken examples.

--- bloc_flux/lib/src/serializers/serializers.dart
+++ bloc_flux/lib/src/serializers/serializers.dart
@@ -1,7 +1,6 @@
 library serializers;
 
 import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
 
